20c28354300fd68f7bf936313a488948d456b88b
seoyeon-jung <jungluchia1204@gmail.com>
2025-06-27 22:35:02 +0900

    style: 캘린더 디자인 수정 (#3)

--- src/components/RentalCalendar.tsx
+++ src/components/RentalCalendar.tsx
@@ -1,5 +1,6 @@
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
+import "../App.css";
 
 const RentalCalendar = ({
   value,
@@ -8,13 +9,22 @@ const RentalCalendar = ({
   value: Date;
   onChange: (date: Date) => void;
 }) => (
-  <Calendar
-    value={value}
-    onChange={onChange as any}
-    minDate={new Date()}
-    calendarType="gregory"
-    locale="ko-KR"
-  />
+  <div
+    style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      padding: "24px 0",
+    }}
+  >
+    <Calendar
+      value={value}
+      onChange={onChange as any}
+      minDate={new Date()}
+      calendarType="gregory"
+      locale="ko-KR"
+    />
+  </div>
 );
 
 export default RentalCalendar;
