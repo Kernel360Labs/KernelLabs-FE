14aa91e8fca07a59e3c3d4615a50f8ad008adf80
HoyiTT <pjs9177@naver.com>
2025-06-28 07:25:08 +0900

    [feat] 워케이션 장소에 이미지 추가-2

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,5 @@
-// export const API_BASE_URL = "https://uscode.porogramr.site";
-export const API_BASE_URL = "http://localhost:8080";
+export const API_BASE_URL = "https://uscode.porogramr.site";
+// export const API_BASE_URL = "http://localhost:8080";
 
 export const WORK_RECOMMEND_URL = `${API_BASE_URL}/api/work/recommend`;
 export const TOUR_RECOMMEND_URL = `${API_BASE_URL}/vacations`; 
