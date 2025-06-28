cae359d70b67ee800bfe0087835080ceca75bf04
seoyeon-jung <jungluchia1204@gmail.com>
2025-06-28 16:22:03 +0900

    [fix] build 오류 해결

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import WorkationSurveyPage from "./pages/WorkationSurveyPage";
 import TourSurveyPage from "./pages/TourSurveyPage";
 import TourResultPage from "./pages/TourResultPage"; // <-- Import the new TourResultPage
 import RentalPage from "./pages/RentalPage";
-import RentalDetailPage from "./pages/RentalDetailPage";
 import MainLayout from "./components/MainLayout";
 import AboutPage from "./pages/AboutPage";
 import ReservationCheckPage from "./pages/ReservationCheckPage";
@@ -20,7 +19,8 @@ function App() {
           <Route index element={<MainPage />} />
           <Route path="workation-survey" element={<WorkationSurveyPage />} />
           <Route path="tour-survey" element={<TourSurveyPage />} />
-          <Route path="tour-result" element={<TourResultPage />} /> {/* <-- Add this new route */}
+          <Route path="tour-result" element={<TourResultPage />} />{" "}
+          {/* <-- Add this new route */}
           <Route path="rental" element={<RentalPage />} />
           <Route path="rental/:id" element={<RentalPage />} />
           <Route path="about" element={<AboutPage />} />
